4627c3db72115573e56050e3a2db3f7d909126b1
ProfeFs <profefs@outlook.com>
2023-10-24 16:48:43 +0200

    Configurada conexión con postgres

---
+++ src/main/java/edu/cesur/fullstack/persistence/entities/BookEntity.java
@@ -0,0 +1,29 @@
+package edu.cesur.fullstack.persistence.entities;
+
+import java.time.LocalDate;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity(name = "book")
+@Data
+@AllArgsConstructor
+public class BookEntity { 
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE) //para posgres
+	//@GeneratedValue(strategy = GenerationType.IDENTITY) //usado para mysql
+    private Integer id; 
+
+    private String titulo; 
+
+    private String autor; 
+
+    private Boolean reservado; 
+
+} 
