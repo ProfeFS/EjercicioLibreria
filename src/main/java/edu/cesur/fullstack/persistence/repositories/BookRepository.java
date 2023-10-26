8038b7ed6db73e50344eac58cd60498c05c82d6f
ProfeFs <profefs@outlook.com>
2023-10-26 13:06:23 +0200

    JPARepository Wildcards

--- src/main/java/edu/cesur/fullstack/persistence/repositories/BookRepository.java
+++ src/main/java/edu/cesur/fullstack/persistence/repositories/BookRepository.java
@@ -1,5 +1,7 @@
 package edu.cesur.fullstack.persistence.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,8 @@ import edu.cesur.fullstack.persistence.entities.BookEntity;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, Integer>{
-
+	
+	//JPArepository wildcars
+	List<BookEntity> findByAutor(String autor);
+	List<BookEntity> findByAutorContains(String autor);
 }
